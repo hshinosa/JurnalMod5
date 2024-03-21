b0589be8a62f72d4056be153bd670c2e75e59e31
hshino@student.telkomuniversity.ac.id <hashfih12@gmail.com>
2024-03-21 10:44:04 +0700

    Commit 3: Menambahkan Class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Penjumlahan
+{
+    public static T JumlahTigaAngka<T>(T Angka1, T Angka2, T Angka3)
+    {
+        return (dynamic)Angka1 + (dynamic)Angka2 + (dynamic)Angka3;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Penjumlahan.JumlahTigaAngka<long>(13,02,22));
+        SimpleDataBase<long> dataBase = new SimpleDataBase<long>();
+        dataBase.AddNewData(13);
+        dataBase.AddNewData(02);
+        dataBase.AddNewData(22);
+        dataBase.AddNewData(20);
+        dataBase.AddNewData(07);
+        dataBase.AddNewData(9);
+        dataBase.PrintAllData();
+    }
+}
+
+public class SimpleDataBase<T>
+{
+    private List <T> storedData;
+    private List<DateTime> inputData;
+
+    public SimpleDataBase()
+    {
+        storedData = new List <T>();
+        inputData = new List <DateTime>();
+    }
+
+    public void AddNewData(T data)
+    {
+        this.storedData.Add(data);
+        this.inputData.Add(DateTime.Now);
+    }
+    public void PrintAllData()
+    {
+        for (int i = 0; i < storedData.Count; i++)
+        {
+            Console.WriteLine("Data" + i + " berisi: " + this.storedData[i] + 
+                ", yang disimpan pada waktu UTC: " + this.inputData[i]);
+
+        }
+    }
+}
