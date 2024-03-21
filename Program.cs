f2c94deab1b6b67402f8f1f649d4396309e2e33b
hshino@student.telkomuniversity.ac.id <hashfih12@gmail.com>
2024-03-21 10:21:39 +0700

    Commit 2: Menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+public class Penjumlahan
+{
+    public static T JumlahTigaAngka<T>(T Angka1, T Angka2, T Angka3)
+    {
+        
+        return (dynamic)Angka1 + (dynamic)Angka2 + (dynamic)Angka3;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Penjumlahan.JumlahTigaAngka<long>(13,02,22));
+    }
+}
